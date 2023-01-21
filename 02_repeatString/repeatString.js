cb79424087f7c48c195ceff0717d18e5dece5b71
sndlmncwango <sndlmncwango@gmail.com>
2023-01-21 12:58:32 +0200

    repeat string exc, got stuck a bit on the returning the variable but got through it

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+let str = ""
+const repeatString = function(say,num) {
+    for(let i=0;i<num;i++){
+        str += say
+    }
+    return str
 };
 
 // Do not edit below this line
