38515ca64d4c9442af9aa8b371494afef4c2d73e
sndlmncwango <sndlmncwango@gmail.com>
2023-01-23 21:35:46 +0200

    project complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,7 @@
 const removeFromArray = function(arr, ...removeEntry) {
     myArr = arr 
     for(let i=0;i<removeEntry.length;i++){
-        if(arr.indexOf(removeEntry[i]) != -1){
+        if(arr.indexOf(removeEntry[i]) !== -1){
             rmIndex = arr.indexOf(removeEntry[i]);
             arr.splice(rmIndex,1)   
         }
