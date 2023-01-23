6d5f7b8a756baa19245387195d13fa88911c1dc5
sndlmncwango <sndlmncwango@gmail.com>
2023-01-23 07:13:56 +0200

    strinf reverse works for only single string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let myStr = [];
+    for(let i=0;i<str.length;i++){
+        myStr.unshift(str[i]);
+    }
+    let output = myStr.toString()
+    return output.split(",").join("")
 };
 
 // Do not edit below this line
