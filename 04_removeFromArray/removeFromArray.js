39f953618d51f4a2405f1d8484a1cce02594dabb
sndlmncwango <sndlmncwango@gmail.com>
2023-01-23 21:25:51 +0200

    add if statement, block code only runs when value is present

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,11 @@
 const removeFromArray = function(arr, ...removeEntry) {
-    myArr = arr    
+    myArr = arr 
     for(let i=0;i<removeEntry.length;i++){
+        if(arr.indexOf(removeEntry[i]) != -1){
             rmIndex = arr.indexOf(removeEntry[i]);
-            arr.splice(rmIndex,1)
-           
-            
+            arr.splice(rmIndex,1)   
         }
+    }
     return myArr
 };
 
