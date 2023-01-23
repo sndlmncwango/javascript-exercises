7d8ed8a8e6adc9eb4bff469e957eae492dcbd26e
sndlmncwango <sndlmncwango@gmail.com>
2023-01-23 07:52:54 +0200

    reverseString - removed the push to an array. set the str to be a string to ensure numbers are turned to string. then i split, reverse and join

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,7 @@
 const reverseString = function(str) {
-    let myStr = [];
-    for(let i=0;i<str.length;i++){
-        myStr.unshift(str[i]);
-    }
-    let output = myStr.toString()
-    return output.split(",").join("")
+   let myStr = str.toString()
+   let output = myStr.split("").reverse().join("")
+   return output
 };
 
 // Do not edit below this line
