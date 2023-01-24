f21b7ef57dd40a25bc3751128ec495f06da9a527
sndlmncwango <sndlmncwango@gmail.com>
2023-01-24 08:50:16 +0200

    sumall - nested if statements to evaluate each condition

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,16 +1,21 @@
-const sumAll = function(startNum,endNum) {
-let totalSum = 0;
-if(startNum<endNum){
-for(let i=startNum;i<=endNum;i++){
-    totalSum += i;
-    }
-}else{
-    for(let i=endNum;i<=startNum;i++){
-        totalSum += i;
-        }
+const sumAll = function (startNum, endNum) {
+    let totalSum = 0;
+    if (startNum > 0 && endNum > 0) {
+        if (typeof (startNum) === "number" && typeof (endNum) === "number") {
+            if (startNum < endNum) {
+                for (let i = startNum; i <= endNum; i++) {
+                    totalSum += i;
+                }
+            } else {
+                for (let i = endNum; i <= startNum; i++) {
+                    totalSum += i;
+                }
+            }
+        } else { return "ERROR"; };
+        return totalSum
+
+    } else { return "ERROR"; };
 }
-return totalSum
-};
 
 // Do not edit below this line
 module.exports = sumAll;
