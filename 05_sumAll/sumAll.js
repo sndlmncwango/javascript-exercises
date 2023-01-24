a56e755318895e76f1b06371d864cbfe80f5dfff
sndlmncwango <sndlmncwango@gmail.com>
2023-01-24 07:28:17 +0200

    sumall- using the for loop. i is the starting number and iletarating until the endnumber.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function(startNum,endNum) {
+let totalSum = 0;
+for(let i=startNum;i<=endNum;i++){
+    totalSum += i;
+}return totalSum
 };
 
 // Do not edit below this line
