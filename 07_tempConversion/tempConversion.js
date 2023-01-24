85abf2939d0213ab43db5dd398ce7f6d91149402
sndlmncwango <sndlmncwango@gmail.com>
2023-01-24 22:08:42 +0200

    used .toFixed() to set the decimal to 1 digit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  sumC = (temp-32)*(5/9);
+  return parseFloat(sumC.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  divide = 9/5
+  sumF = temp * divide;
+  sumF += 32;
+  return parseFloat(sumF.toFixed(1))
 };
 
 // Do not edit below this line
