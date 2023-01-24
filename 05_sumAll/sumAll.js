8fdcd211a9e6e5b682e35aa0a522b7a51f26750a
sndlmncwango <sndlmncwango@gmail.com>
2023-01-24 08:13:32 +0200

    used IF statement to change which number is the base when using the for loop. if startnum >endnum the its set is a base

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,15 @@
 const sumAll = function(startNum,endNum) {
 let totalSum = 0;
+if(startNum<endNum){
 for(let i=startNum;i<=endNum;i++){
     totalSum += i;
-}return totalSum
+    }
+}else{
+    for(let i=endNum;i<=startNum;i++){
+        totalSum += i;
+        }
+}
+return totalSum
 };
 
 // Do not edit below this line
