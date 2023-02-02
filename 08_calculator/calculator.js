c8cc9daab4f50dc492ec32923de6b486ed37620f
sndlmncwango <sndlmncwango@gmail.com>
2023-02-02 07:52:03 +0200

    all calculator fuctions passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	let sum = 0;
+  sum = num1 += num2
+  return sum
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	let sum = 0;
+  sum = num1 -= num2
+  return sum
 };
 
-const sum = function() {
-	
+const sum = function([...num]) {
+  const myArr = num;
+  if(myArr !== []){
+    return parseInt(myArr.reduce((accu,curr)=>{
+      return accu + curr
+    },0))
+  }else{return 0}
 };
 
-const multiply = function() {
-
+const multiply = function([...num]) {
+  const myArr = num
+  return myArr.reduce((accumulator,currentValue)=>{
+    return accumulator * currentValue
+  })
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+	return num1 ** num2
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fact = [];
+  if(num !== 0){
+  for(let i=num;i>=1;i--){
+    fact.push(i)
+  }return fact.reduce((accumulator,currentvalue)=>{
+      return accumulator * currentvalue
+  })}else{return 1}
 };
 
 // Do not edit below this line
