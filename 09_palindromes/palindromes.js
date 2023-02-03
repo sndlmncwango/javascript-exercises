10ab818176ef68add44f10bd867e7d9cc0dfaebf
sndlmncwango <sndlmncwango@gmail.com>
2023-02-03 08:05:11 +0200

    program does not resolve for multiple words. Still need a way to remove punctuation

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let myWord = word.toLowerCase()
+    if (myWord.includes("!")) {
+        let newword = myWord.replace("!", "");
+        sWord = newword.split("").reverse().join("");
+        if(newword == sWord){
+            return true
+        }
+    } else
+        if (myWord != myWord.includes("!")) {
+            let pWord = myWord.split("").reverse().join("");
+            if (myWord == pWord) {
+                return true
+            } else {return false}
+        }
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
