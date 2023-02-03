2d324d8419bdcf5b2dc466291a7370dbaeca576b
sndlmncwango <sndlmncwango@gmail.com>
2023-02-03 08:16:24 +0200

    used newWord string to remove all punctuaction on the work, reverse the word then compare from origina word

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,19 +1,11 @@
 const palindromes = function (word) {
-    let myWord = word.toLowerCase()
-    if (myWord.includes("!")) {
-        let newword = myWord.replace("!", "");
-        sWord = newword.split("").reverse().join("");
-        if(newword == sWord){
-            return true
-        }
-    } else
-        if (myWord != myWord.includes("!")) {
-            let pWord = myWord.split("").reverse().join("");
-            if (myWord == pWord) {
-                return true
-            } else {return false}
-        }
-};
+    let lowcap = word.toLowerCase()
+    let newWord = lowcap.replace(/[^A-Za-z0-9]/g,"");
+    let revWord = newWord.split("").reverse().join("");
+    if(newWord == revWord){
+        return true
+    }else{return false}
+}
 
 // Do not edit below this line
 module.exports = palindromes;
